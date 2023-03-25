d9df4511a6679e030ca41b9e668ef02fa099b1df
Tareq Harh <tareq310harh@gmail.com>
2023-03-25 17:16:56 +0300

    all tests passed

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalizedWords.join(' ');
+  });
 }
